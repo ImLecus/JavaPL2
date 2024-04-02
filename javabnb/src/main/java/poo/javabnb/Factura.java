f2b963213da650f5ffe2e6f0813066a36837e62d
ImLecus <marcosgf.dev@gmail.com>
2024-04-02 09:26:28 +0200

    Extended bill example

---
+++ javabnb/src/main/java/poo/javabnb/Factura.java
@@ -0,0 +1,19 @@
+package poo.javabnb;
+
+public class Factura {
+    /**
+     * TO-DO: Generar archivo, crear factura dinámica, aplicar texto al archivo
+     */
+    public static void generarFactura(Cliente c) {
+        String content = "┌------------------------------------------┐\r\n" + //
+                         "|                  FACTURA                 |\r\n" + //
+                         "├##########################################┤\r\n" + //
+                         "|                                          |\r\n" + //
+                         "| Datos del inmueble                       |\r\n" + //
+                         "| ------------------                       |\r\n" + //
+                         "| Nombre:                                  |\r\n" + //
+                         "|                                          |\r\n" + //
+                         "└------------------------------------------┘"
+                         ;
+    }
+}
