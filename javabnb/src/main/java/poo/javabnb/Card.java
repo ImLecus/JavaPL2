ca4da09bf34bcf66a321f56d8066657a9cde2507
ImLecus <marcosgf2005@gmail.com>
2024-04-09 15:22:43 +0200

    upgrade: converted Card from class to record

--- javabnb/src/main/java/poo/javabnb/Card.java
+++ javabnb/src/main/java/poo/javabnb/Card.java
@@ -1,24 +1,15 @@
 package poo.javabnb;
 
-class Card {
-    private final String owner;
-    private final String number;
-    private final String endDate;
-    private final String cvv;
-
+record Card(String owner, String number, String endDate, String cvv) {
     /**
      * Cards are used by the particulars to pay the buildings.
-     * @param owner the name of the card owner.
-     * @param number the card's number.
+     *
+     * @param owner   the name of the card owner.
+     * @param number  the card's number.
      * @param endDate the card's end date of validation
-     * @param cvv the card's CVV.
+     * @param cvv     the card's CVV.
      **/
-    public Card(String owner, String number, String endDate, String cvv) {
-        this.owner = owner;
-        this.number = number;
-        this.endDate = endDate;
-        this.cvv = cvv;
-    }
+    public Card {}
 
     /**
      * @return all the card information packaged into an array of Strings.
