dbc3c395cbf7c7b14cad5307aa7d8feb4617afc1
ImLecus <marcosgf2005@gmail.com>
2024-04-09 14:07:27 +0200

    upgrade: converted Range from class to record

--- javabnb/src/main/java/poo/javabnb/Range.java
+++ javabnb/src/main/java/poo/javabnb/Range.java
@@ -1,22 +1,17 @@
 package poo.javabnb;
 
-public class Range {
-    public final int min;
-    public final int max;
-
+public record Range(int min, int max) {
     /**
      * Simple representation of a range.
+     *
      * @param min minimum value
      * @param max maximum value
      */
-    public Range(int min, int max) {
-        this.min = min;
-        this.max = max;
-    }
+    public Range {}
 
     /**
-     * @return if the value is in the range bounds.
      * @param value value to check
+     * @return if the value is in the range bounds.
      */
     public boolean isInRange(int value) {
         return value >= min && value <= max;
