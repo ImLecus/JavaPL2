5e64d76d6183eb26e4e41e8f92a45bb2b390035f
ImLecus <marcosgf2005@gmail.com>
2024-04-09 13:18:20 +0200

    bug-fix: solved login form problems running

--- javabnb/src/main/java/gui/Login.java
+++ javabnb/src/main/java/gui/Login.java
@@ -7,4 +7,16 @@ public class Login {
     private JPasswordField passwordField1;
     private JButton registrarseButton;
     private JButton iniciarSesiónButton;
+    private JPanel panel;
+
+    public static void main(String[] args){
+        Login login = new Login();
+        JFrame frame = new JFrame("Login");
+
+        frame.setContentPane(login.panel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
