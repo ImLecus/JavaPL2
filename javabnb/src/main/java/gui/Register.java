c18a1bc9df930e70de69e0fafdf97137c72f923d
Promete04 <ramossantosguillermo14@gmail.com>
2024-04-09 11:40:30 +0200

    gui: added register draft

---
+++ javabnb/src/main/java/gui/Register.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+public class Register {
+    private JTextField exampleExampleComTextField;
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JCheckBox aceptaLosTérminosYCheckBox;
+    private JButton registrarseButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
