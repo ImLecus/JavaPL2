b699c029e532037d9740f3213ff292f4378804e9
ImLecus <marcosgf2005@gmail.com>
2024-04-13 12:20:33 +0200

    upgrade: improved App code legibility

--- javabnb/src/main/java/gui/App.java
+++ javabnb/src/main/java/gui/App.java
@@ -1,5 +1,4 @@
 package gui;
-import poo.javabnb.Client;
 import poo.javabnb.DataBase;
 import poo.javabnb.Session;
 
@@ -11,39 +10,40 @@ public class App {
     public static DataBase db = new DataBase();
     public static Session session = null;
 
-    private static CardLayout cardLayout = new CardLayout();
-    private static JPanel cards = new JPanel(cardLayout);
+    private static final CardLayout cardLayout = new CardLayout();
+    private static final JPanel cards = new JPanel(cardLayout);
 
-    private static Login login = new Login();
-    private static Register register = new Register();
-    private static Main main = new Main();
+    private static final Login login = new Login();
+    private static final Register register = new Register();
+    private static final Main main = new Main();
 
     public static void main(String[] args){
         cards.add(login.panel, "LOGIN");
         cards.add(register.panel, "REGISTER");
         cards.add(main.panel, "MAIN");
 
-        Client juan = new Client("003333030E", "Juan", "juan@gmail.com", "juanito123", "696696696");
-        Client dani = new Client("003333030E", "Dani", "dani@gmail.com", "jdd123", "696696696");
-        db.add(juan);
-        db.add(dani);
-
-        JFrame frame = new JFrame("App");
-        frame.setContentPane(cards);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setMinimumSize(new Dimension(310, 425));
-        frame.setVisible(true);
-
+        setupFrame();
         redirect("LOGIN");
     }
 
     /**
      * Pages are inside a CardLayout. Inside the page's procedure, they can
-     * call the App.redirect() function to change the view.
+     * call the App.redirect() method to change the view.
      * @param page A string with the page name.
      */
     public static void redirect(String page){
         cardLayout.show(cards, page);
+    }
+
+    /**
+     * setupFrame() method prepares the JFrame, making it visible, adding the title and
+     * setting the minimum resizeable size.
+     */
+    public static void setupFrame(){
+        JFrame frame = new JFrame("JavaB&B");
+        frame.setContentPane(cards);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(310, 425));
+        frame.setVisible(true);
     }
 }
