62c1e1689136635357c0a864e5a15717fb6e6b23
Promete04 <ramossantosguillermo14@gmail.com>
2024-04-13 18:33:57 +0200

    gui: changed EULA structure

--- javabnb/src/main/java/gui/EULA.java
+++ javabnb/src/main/java/gui/EULA.java
@@ -11,30 +11,24 @@ public class EULA {
     public JPanel panel;
     private JScrollPane eulaScroll;
     private JLabel titleEula;
-    private JButton aceptarButton;
-    private JButton rechazarButton;
+    private JButton leidoButton;
     private JLabel eulaText;
 
     public EULA() {
         titleEula.setBorder(null);
         eulaScroll.setBorder(null);
         FontManager.useFont(titleEula, FontManager.titleFont, Color.BLACK);
-        FontManager.useFont(aceptarButton, FontManager.boldFont, Color.black);
-        FontManager.useFont(rechazarButton, FontManager.boldFont, Color.black);
-        aceptarButton.setBorder(null);
-        rechazarButton.setBorder(null);
-        aceptarButton.addActionListener(new ActionListener() {
+        FontManager.useFont(leidoButton, FontManager.boldFont, Color.black);
+        FontManager.useFont(eulaText, FontManager.regularFont, Color.BLACK);
+        leidoButton.setBorder(null);
+
+
+        leidoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 App.redirect("REGISTER");
             }
         });
-        rechazarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                App.session.endSession();
-                App.redirect("LOGIN");
-            }
-        });
     }
-}
+    }
+
