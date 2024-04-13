1ec285d764b05024b232b9662803ba24b0f9d9b1
Promete04 <ramossantosguillermo14@gmail.com>
2024-04-13 17:33:42 +0200

    gui: setting window size change

--- javabnb/src/main/java/gui/App.java
+++ javabnb/src/main/java/gui/App.java
@@ -39,7 +39,7 @@ public class App {
      */
     public static void redirect(String page){
         if(page.equals("MAIN")){
-            frame.setMinimumSize(new Dimension(540, 560));
+            frame.setSize(540, 560);
         }
         if(page.equals("REGISTER")){
             frame.setSize(320, 550);
@@ -59,7 +59,7 @@ public class App {
         frame.setIconImage(img.getImage());
         frame.setContentPane(cards);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(320, 450));
+        frame.setMinimumSize(new Dimension(540, 560));
         frame.setVisible(true);
     }
 
