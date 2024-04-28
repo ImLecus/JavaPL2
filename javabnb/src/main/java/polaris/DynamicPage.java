fc28e3f1d1c4c96618427416400d033f8e34f5a9
Marcos <marcosgf.dev@gmail.com>
2024-04-28 11:07:53 +0200

    update: added new content into DynamicPage

--- javabnb/src/main/java/polaris/DynamicPage.java
+++ javabnb/src/main/java/polaris/DynamicPage.java
@@ -1,11 +1,19 @@
 package polaris;
 
+import java.util.ArrayList;
+
 /**
  * Interface for all the pages that
  * changes its content while the program
  * executes.
  */
 public interface DynamicPage {
+    ArrayList<Object> items = new ArrayList<>();
+    default void reload(){
+        deleteDynamicContent();
+        reloadContent();
+        createDynamicContent();
+    }
     /**
      * Reloads all the existing content changing their data.
      * To be used with dynamic content, deleteDynamicContent()
