2b96267bbd3cb5621f2c86b9dfda6c29fe1f3808
Marcos <marcosgf.dev@gmail.com>
2024-05-21 14:50:22 +0200

    extra: deleted unused content

--- javabnb/src/main/java/poo/javabnb/exceptions/NullBuildingException.java
+++
@@ -1,7 +0,0 @@
-package poo.javabnb.exceptions;
-
-public class NullBuildingException extends Exception {
-    public NullBuildingException(){
-        super("The building does not exist.");
-    }
-}
